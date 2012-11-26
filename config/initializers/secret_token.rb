80377a11377c3c6e29b6e81dab2f5b00f9cbae6c
Chloe Roger <chloe.roger@intellicore.net>
2012-11-26 10:23:22 +0100

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChronoTime::Application.config.secret_token = 'fb6d1118200b9900ffdaa2d78acc5dea05433225cf295c5f632f16be64d41603a7637a25b101a6330ebfda6e24a6476448b18775935a6a1b0c9a55463379b0f3'
